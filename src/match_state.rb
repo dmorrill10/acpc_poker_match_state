5a8e99a0ddb79feb9768427cfd14bfb1c0a08d09
Dustin Morrill <morrill@ualberta.ca>
2011-12-31 12:52:30 -0700

    Fixed a bug.

--- src/match_state.rb
+++ src/match_state.rb
@@ -52,12 +52,12 @@ class MatchState
       else
          update_state_of_players!
          evaluate_end_of_hand! if hand_ended?
+         @player_acting_sequence[@match_state_string.round] << player_who_acted_last.seat
+         @betting_sequence[@match_state_string.round] << @match_state_string.last_action
          if @match_state_string.round > @last_round
             @player_acting_sequence << []
             @betting_sequence << []
          end
-         @player_acting_sequence[@match_state_string.round] << player_who_acted_last.seat
-         @betting_sequence[@match_state_string.round] << @match_state_string.last_action
       end
       
       self
