e592485edad17bc6781c0a5457f2d98166288f71
Dustin Morrill <morrill@ualberta.ca>
2012-06-03 19:59:14 -0600

    Fixed a bug in the gem module file.

--- lib/acpc_poker_match_state.rb
+++ lib/acpc_poker_match_state.rb
@@ -1,4 +1,4 @@
-require "../acpcp_poker_match_state/version"
+require File.expand_path("../acpcp_poker_match_state/version", __FILE__)
 
 require File.expand_path("../acpcp_poker_match_state/acpc_poker_match_state_defs", __FILE__)
 require File.expand_path("../acpcp_poker_match_state/match_state_transition", __FILE__)
